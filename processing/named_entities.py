471ff7c44748e87efbb11436e9a10a216c515139
Nikolay Petrukhin <npetrukhin@osinit.com>
2017-10-28 17:23:56 +0300

    Added examples

--- processing/named_entities.py
+++ processing/named_entities.py
@@ -1,8 +1,11 @@
+from pprint import pprint
+
 from natasha import (NamesExtractor, DatesExtractor, MoneyExtractor, LocationExtractor, AddressExtractor,
                      OrganisationExtractor, PersonExtractor)
 
 
 class NamedEntitiesExtractorsLoader:
+    extractors = []
     date = None
     money = None
     location = None
@@ -11,6 +14,7 @@ class NamedEntitiesExtractorsLoader:
     person = None
 
     def __init__(self):
+        self.extractors = ['date', 'money', 'location', 'address', 'organization', 'person']
         self.date = DatesExtractor()
         self.name = NamesExtractor()
         self.money = MoneyExtractor()
@@ -29,3 +33,20 @@ class NamedEntitiesExtractorsLoader:
             })
         return result
 
+    def extract_all(self, text):
+        result = []
+        for extractor in self.extractors:
+            result.extend(self.extract(text, extractor))
+        return result
+
+
+if __name__ == '__main__':
+    extractors = NamedEntitiesExtractorsLoader()
+    text = '''
+    Простите, еще несколько цитат из приговора. одна копейка «…Отрицал существование
+    Иисуса и пророка Мухаммеда», «наделял Иисуса Христа качествами
+    ожившего мертвеца — зомби» [и] «качествами покемонов —
+    представителей бестиария японской мифологии, тем самым совершил
+    преступление, предусмотренное статьей 148 УК РФ от 1 декабря 2017 года
+    '''
+    pprint(extractors.extract_all(text))
--- processing/speller.py
+++ processing/speller.py
@@ -5,5 +5,14 @@ class Speller(YandexSpeller):
     def process(self, text):
         spelled_text = self.spell(text)
         for item in spelled_text:
+            print(item)
             text = text.replace(item['word'], item['s'][0])
         return text
+
+
+if __name__ == '__main__':
+    text = """
+    я вшел из дома кагда во всех онах
+    """
+    speller = Speller()
+    print(speller.process(text))
