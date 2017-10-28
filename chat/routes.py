6387bc486177aeea979fd86bfc6baf0473eeb445
totaki <karagabul@gmail.com>
2017-10-28 18:22:06 +0300

    Update int

--- chat/routes.py
+++ chat/routes.py
@@ -4,4 +4,5 @@ routes = [
     (r'/api/v1/status', StatusHandler),
     (r'/api/v1/webhook/in', InWebhookHadler),
     (r'/api/v1/webhook/out', OutWebhookHadler),
+    (r'/api/v1/drop', DropStateHandler),
 ]
