8e8b144f3a5cbffa5514ba5270ad9fc8e826344a
Nikolay Petrukhin <npetrukhin@osinit.com>
2017-10-29 14:19:37 +0300

    Added websocket handler

--- backend/handlers.py
+++ backend/handlers.py
@@ -1,10 +1,12 @@
 import json
 
 import tornado.web
+from tornado import websocket, ioloop
 from tornado.gen import coroutine
 
 from mapper import mapper
 
+live_web_sockets = set()
 
 class PipelinesHandler(tornado.web.RequestHandler):
 
@@ -24,5 +26,34 @@ class PipelinesHandler(tornado.web.RequestHandler):
                 text_object = yield mapper[processor].process(text_object)
             else:
                 text_object = mapper[processor].process(text_object)
-        print(text_object)
-        self.finish(json.dumps(text_object))
+        result = json.dumps(text_object)
+        ioloop.IOLoop.current().add_callback(
+            PipelinesWebSocketHandler.web_socket_send_message, result)
+        self.finish(result)
+
+
+class PipelinesWebSocketHandler(websocket.WebSocketHandler):
+    def open(self):
+        self.set_nodelay(True)
+        live_web_sockets.add(self)
+        self.write_message("Welcome to TNLP")
+
+    def on_message(self, message):
+        print(message)
+
+    def on_close(self):
+        print("WebSocket closed")
+
+    @staticmethod
+    def web_socket_send_message(message):
+        removable = set()
+        for ws in live_web_sockets:
+            if not ws.ws_connection or not ws.ws_connection.stream.socket:
+                removable.add(ws)
+            else:
+                ws.write_message(message)
+        for ws in removable:
+            live_web_sockets.remove(ws)
+
+    def check_origin(self, origin):
+        return True
